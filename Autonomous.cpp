ce2f8a1e3b391f0a6915f1dd2f671f29d2c57ac0
Communist Cosmonaut <Jas Delgado>
2015-10-30 18:40:49 -0700

    updated autonomous files, which are IN PROGRESS. added one of the autonomous functions -- shoot shoot drive

--- Autonomous.cpp
+++ Autonomous.cpp
@@ -1,1 +1,56 @@
 #include "Autonomous.h"
+
+
+Autonomous::Autonomous()
+{
+	armManip = new ManipArm();
+}
+
+Autonomous::~Autonomous()
+{
+
+}
+
+void Autonomous::shootShootDrive()
+{
+	//ignores hot goal, 2 ball auton (shoot, shoot, drive)
+	if(timer->Get() > 0 + autonInitTime && timer->Get() < 1 + autonInitTime)
+	{
+		armManip->moveArm(true, false);
+	}
+	if(timer->Get() > 1.2 + autonInitTime && timer->Get() < 1.5 + autonInitTime)
+	{
+		launcher->autoFirstLaunch();
+	}
+	if(timer->Get() > 1.5 + autonInitTime && timer->Get() < 3.5  + autonInitTime)
+	{
+		launcher->autoReset();
+	}
+	if(timer->Get() > 3.5 + autonInitTime && timer->Get() < 5 + autonInitTime)
+	{
+		manipulator->intakeRoller->Set(1);
+	}
+	if(timer->Get() > 5 + autonInitTime && timer->Get() < 6 + autonInitTime)
+	{
+		manipulator->intakeRoller->Set(0);
+		armManip->moveArm(false, true);
+	}		
+	if(timer->Get() > 6.2 + autonInitTime && timer->Get() < 7.2 + autonInitTime)
+	{
+		manipulator->intakeRoller->Set(1);
+		armManip->moveArm(true, false);
+	}
+	if(timer->Get() > 7.4 + autonInitTime && timer->Get() < 7.7 + autonInitTime)
+	{
+		manipulator->intakeRoller->Set(0);
+		launcher->autoSecondLaunch();
+	}
+	if(timer->Get() > 7.8 + autonInitTime && timer->Get() < 9.8 + autonInitTime)
+	{
+		drive->setLinVelocity(-1);
+	}
+	if(timer->Get() > 9.8 + autonInitTime && timer->Get() < 10 + autonInitTime)
+	{
+		drive->setLinVelocity(0);
+	}
+}
